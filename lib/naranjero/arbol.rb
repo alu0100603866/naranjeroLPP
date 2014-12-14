3c157a77af2ec3945c1f6e9d30c724c6b9fb765c
Juan Tareq <juantareq@MacBook-Pro-de-Juan-2.local>
2014-12-14 00:54:53 +0000

    Con el metodo uno_mas el arbol crece

--- lib/naranjero/arbol.rb
+++ lib/naranjero/arbol.rb
@@ -1,9 +1,22 @@
 class Arbol
     attr_reader :edad, :altura, :contador
+    
     def initialize()
         @edad = 0
         @altura = 0
         @contador = 0
+    end
+    
+    def uno_mas
+        @edad += 1
+        @altura += 0.5
+        @contador = produccion()
+    end
+    
+    def produccion
+        if (@edad > 4)
+            @contador += rand(edad)
+        end
     end
 
 end
--- spec/naranjero_spec.rb
+++ spec/naranjero_spec.rb
@@ -10,4 +10,11 @@ describe 'Naranjero' do
         expect(@Naranjero.altura).to eq(0)
         expect(@Naranjero.edad).to eq(0)
 	end
+    
+    it 'Debe haber un metodo que incremente la edad' do
+        @Naranjero.uno_mas
+        @Naranjero.uno_mas
+        expect(@Naranjero.edad).to eq(2)
+        expect(@Naranjero.altura).to eq(1)
+    end
 end
